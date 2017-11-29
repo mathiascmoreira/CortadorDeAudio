d4310da30d6d8cd0c21f878189a81fa434fbe55d
Mathias Moreira <mathias.moreira@inventsoftware.com.br>
2017-11-29 17:06:58 -0200

    implementando

---
+++ CortadorDeAudio/IAudioTools.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CortadorDeAudio
+{
+    public interface IAudioTools
+    {
+        void TrimAudio(string filePath, string savePath, TimeSpan from, TimeSpan to);
+    }
+}
